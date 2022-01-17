2b573f1319316c1cf31a07b5eb87f68e392028de
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-17 18:27:56 -0500

    built most middlewares and register post route, 16 tests passing

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -48,6 +48,7 @@ function findById(user_id) {
     .join("roles as r", "u.role_id", "r.role_id")
     .select("u.user_id", "u.username", "r.role_name")
     .where("u.user_id", user_id)
+    .first()
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
